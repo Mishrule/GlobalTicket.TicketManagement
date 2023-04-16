807336d6e49c4b6af5cdc3eb5706876306bdd938
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2023-04-16 21:56:02 +0000

    Add project files.

---
+++ GlobalTicket.TicketManagement.Application/Features/Categories/Queries/GetCategoriesListWithEvents/CategoryEventListVm.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents
+{
+    public class CategoryEventListVm
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<CategoryEventDto>? Events { get; set; }
+    }
+}
---
+++ GlobalTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlobalTicket.TicketManagement.Domain.Common;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Category:AuditableEntity
+    {
+        public Guid CategoryId {get; set; }
+        public string Name{get; set; }= string.Empty;
+        public ICollection<Event>? Events { get; set; }
+
+    }
+}
